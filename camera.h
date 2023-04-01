02efeb77a2409a95222629111f31117457b804b2
azamatshsh123@gmail.com <azamatshsh123@gmail.com>
2023-04-01 15:50:29 +0300

    урок 16(проблема с масштабированием окна)

--- camera.h
+++ camera.h
@@ -3,7 +3,7 @@
 
 #include "math_3d.h"
 
-
+//const double M_PI = 3.14;
 class Camera
 {
 public:
@@ -47,11 +47,6 @@ private:
 
     float m_AngleH;
     float m_AngleV;
-
-    bool m_OnUpperEdge;
-    bool m_OnLowerEdge;
-    bool m_OnLeftEdge;
-    bool m_OnRightEdge;
 
     Vector2i m_mousePos;
 };
